9d65a28f8252ade46c797c37e471059d271335d0
emreharman <emre.harman@ogr.sakarya.edu.tr>
2022-02-08 02:46:48 +0300

    make search request after login

--- src/screens/BrowseScreen.js
+++ src/screens/BrowseScreen.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import axios from 'axios';
+import {useSelector} from 'react-redux';
 
 const BrowseScreen = () => {
+  const loginState = useSelector(state => state.loginState);
+  useEffect(() => {
+    console.log(loginState);
+    axios
+      .get('https://nox-podcast-api.vercel.app/search', {
+        headers: {
+          Authorization: `Bearer ${loginState.token}`,
+        },
+      })
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
   return (
     <View>
       <Text>Browse screen</Text>
